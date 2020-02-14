f014db1a2456cc15bd6f836c0beb1b7b4aff933a
komissar75 <alexander@melnikov.name>
2020-02-14 14:45:06 +0300

    w

--- ip_test.py
+++ ip_test.py
@@ -5,7 +5,7 @@ import subprocess
 meshcmd_path = "/home/komissar/Downloads/meshcmd"
 amt_user = "admin"
 amt_password = "P@ssw0rd"
-
+amt_version = ""
 amt_status ="" #activated or not
 amt_ip_mode = "" #static or dynamic
 amt_mac = ""
@@ -21,6 +21,10 @@ def net_meshcmd():
     result = subprocess.run([meshcmd_path, 'AmtInfo'], stdout=subprocess.PIPE)
     #print(result.stdout.decode())
     amt_result_list = result.stdout.decode().splitlines()
+    amt_version = amt_result_list[0].split(",")[0]
+    amt_status = amt_result_list[0].split(",")[1].strip()
+    print("AMT version: " + amt_version)
+    print("AMT status: " + amt_status)
     #print(amt_result_list)
     #net_result = subprocess.run([meshcmd_path, 'AmtNetwork', '--user', amt_user, '--password', amt_password], stdout=subprocess.PIPE)
     #print(type(net_result.stdout.decode()))
@@ -28,9 +32,9 @@ def net_meshcmd():
     sys_result_list = subprocess.run(['ip', '-o', '-f', 'inet', 'address'], stdout=subprocess.PIPE).stdout.decode().splitlines()
     #print(sys_result_list[0][3:5])
     if len(sys.argv) == 1:
-        print("AMT status is:")
-        for i in amt_result_list:
-            print(i)
+        #print("AMT status is:")
+        #for i in amt_result_list:
+        #    print(i)
         print("Loclal system IP conf is:")
         for i in sys_result_list:
             if i[3:5] != "lo":
